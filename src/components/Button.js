b2f8de1b2b428d639011fd217c30b41f45c3bd69
Safia Abdale <safia.abdale@gmail.com>
2022-01-18 20:43:13 -0500

    Added a Danger story for buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,9 +5,16 @@ import "components/Button.scss";
 export default function Button(props) {
    let buttonClass = "button";
 
+   // Confirm story
    if (props.confirm) {
      buttonClass += " button--confirm";
    }
+
+   // Danger story
+   if (props.danger) {
+      buttonClass += " button--danger";
+   }
+
 
    return (
       <button className={buttonClass}>{props.children}</button>
