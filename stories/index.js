0f746422e87fae4ebdfb22ac767b70b0f025d86e
Safia Abdale <safia.abdale@gmail.com>
2022-01-19 13:50:10 -0500

    Added story for InterviewerListItem

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,7 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "../src/components/DayListItem";
 import DayList from "../src/components/DayList";
+import InterviewerListItem from "../src/components/InterviewerListItem";
 
 
 
@@ -75,4 +76,41 @@ storiesOf("DayList", module)
   ))
   .add("Wednesday", () => (
       <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+  ));
+
+
+
+//  InterviewerListItem
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
   ));
