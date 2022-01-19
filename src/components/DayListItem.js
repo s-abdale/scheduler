e5917978c72f038f3133a17d3ace8b8c2a5638f6
Safia Abdale <safia.abdale@gmail.com>
2022-01-18 22:48:16 -0500

    Added test for DayListItems and made the h3 attribute variable

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -12,13 +12,25 @@ export default function DayListItem(props) {
     "day-list__item--full": (props.spots === 0 && true )
   });
 
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return "no spots remaining"
+    } else if (props.spots === 1) {
+      return "1 spot remaining"
+    } else {
+      return `${props.spots} spots remaining`
+    }
+  };
+
   return (
     <li 
       className={dayClass}
       onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      
+      <h3 className="text--light">{formatSpots()}</h3>
+      
     </li>
   );
 }
