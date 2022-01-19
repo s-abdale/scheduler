67a61444819881d049f93a6f6cfcc5a9a4d31e1f
Safia Abdale <safia.abdale@gmail.com>
2022-01-19 13:55:23 -0500

    Replaced hard-coded values with props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -9,10 +9,10 @@ export default function InterviewerListItem(props) {
     <li className="interviewers__item">
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.name}
     </li>
   );
 }
