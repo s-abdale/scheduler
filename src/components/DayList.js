76aed59151dc51a6b4f515d1e9f99e2310579052
Safia Abdale <safia.abdale@gmail.com>
2022-01-20 16:22:55 -0500

    Updated DayList to reflect changes to value names made in Application.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -12,8 +12,8 @@ export default function DayList(props) {
         key={day.id}
         name={day.name} 
         spots={day.spots} 
-        selected={day.name === props.day}
-        setDay={() => props.setDay(props.name)}  
+        selected={day.name === props.value}
+        setDay={() => props.onChange(props.name)}  
       />
     )
   });
