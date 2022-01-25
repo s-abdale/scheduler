6d50870e123885397b891fd8c7c81a0a0a45f5b0
Safia Abdale <safia.abdale@gmail.com>
2022-01-25 12:21:13 -0500

    Imported Fragment to stories and added an 'Appointment Empty' story. Populated the Appointment component in index.js

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -202,7 +202,13 @@ storiesOf("Appointment", module)
       interviewers={interviewers}
       onSave={action("onSave")}
       onCancel={action("onCancel")}
-    />);
+    />)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+    ));
 
 
 
