8b53654c9670dd08d9285632a7c6b9d212b353f9
Safia Abdale <safia.abdale@gmail.com>
2022-01-25 23:38:47 -0500

    Added edge case handling to function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -6,11 +6,16 @@ export function getAppointmentsForDay(state, day) {
   // loop through to state.days array to find name=day
   daysArray.forEach(availableDay => {
     if (availableDay.name === day){
-      // takes us to the object with the correct day. now loop through appointments and compare each one to the items in appointmentsObj
-      availableDay.appointments.forEach(bookedAppointment => {
-        appointmentsOutput.push(appointmentsObj[bookedAppointment])
-      })
-      
+      // takes us to the object with the correct day
+      if (availableDay.appointments.length === 0) {
+        // validation: what if there are no appointments? return empty array
+        return []
+      } else {
+        // success: now loop through appointments and compare each one to the items in appointmentsObj
+        availableDay.appointments.forEach(bookedAppointment => {
+          appointmentsOutput.push(appointmentsObj[bookedAppointment])
+        })
+      }
     }
   })
   //... returns an array of appointments for that day
