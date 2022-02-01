62caa87e6dfa5b93c5e6d79b7ccf12cd32f55a56
Safia Abdale <safia.abdale@gmail.com>
2022-01-31 23:36:49 -0500

    Implemented the Update, to show a new booking

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,6 +20,18 @@ export default function Application(props) {
   const interivewers = getInterviewersForDay(state, state.day);
   function bookInterview(id, interview) {
     console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    setState({
+      ...state,
+      appointments
+    });
   };
 
 
