2f4ac70ddd7d27825621e1750e6d0b9caffe0f8b
Safia Abdale <safia.abdale@gmail.com>
2022-02-07 21:42:09 -0500

    Implemented first set of tests

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import InterviewerListItem from "./InterviewerListItem";
 
@@ -33,3 +34,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
